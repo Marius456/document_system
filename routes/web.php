bc950fb91f3b18135c9a77b66ecf6f78b13aad1a
Marius456 <marius.zilguzis@ktu.edu>
2021-02-02 13:37:41 +0200

    faulty CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
  
-Route::get('{any}', function () {
-    return view('app');
-})->where('any', '.*');
+// Route::get('{any}', function () {
+//     return view('app');
+// })->where('any', '.*');
+
+// Route::get('/', 'PostController@index');
+use App\Http\Controllers\PostController;
+
+Route::get('/', [PostController::class, 'create']);
