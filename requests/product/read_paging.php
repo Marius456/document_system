5d49b5461170a12ecefd48112750e0bec530aeb4
Marius456 <marius.zilguzis@ktu.edu>
2021-02-15 11:23:12 +0200

    added product CRUD

--- requests/product/read_paging.php
+++ requests/product/read_paging.php
@@ -2,10 +2,15 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
- 
+  
 // include database and object files
-include_once '../config/database.php';
-include_once '../objects/product.php';
+include_once '../../config/core.php';
+include_once '../../shared/utilities.php';
+include_once '../../config/database.php';
+include_once '../../objects/product.php';
+  
+// utilities
+$utilities = new Utilities();
   
 // instantiate database and product object
 $database = new Database();
@@ -15,7 +20,7 @@ $db = $database->getConnection();
 $product = new Product($db);
   
 // query products
-$stmt = $product->read();
+$stmt = $product->readPaging($from_record_num, $records_per_page);
 $num = $stmt->rowCount();
   
 // check if more than 0 record found
@@ -24,6 +29,7 @@ if($num>0){
     // products array
     $products_arr=array();
     $products_arr["records"]=array();
+    $products_arr["paging"]=array();
   
     // retrieve our table contents
     // fetch() is faster than fetchAll()
@@ -46,10 +52,17 @@ if($num>0){
         array_push($products_arr["records"], $product_item);
     }
   
+  
+    // include paging
+    $total_rows=$product->count();
+    $page_url="{$home_url}product/read_paging.php?";
+    $paging=$utilities->getPaging($page, $total_rows, $records_per_page, $page_url);
+    $products_arr["paging"]=$paging;
+  
     // set response code - 200 OK
     http_response_code(200);
   
-    // show products data in json format
+    // make it json format
     echo json_encode($products_arr);
 }
   
@@ -58,7 +71,7 @@ else{
     // set response code - 404 Not found
     http_response_code(404);
   
-    // tell the user no products found
+    // tell the user products does not exist
     echo json_encode(
         array("message" => "No products found.")
     );
