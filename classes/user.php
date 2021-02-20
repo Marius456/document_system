33bfc1db3a4b245c6ac3edcb951a672a1d15e4d9
Marius <marius.zilguzis@ktu.edu>
2021-02-20 15:26:36 +0200

    fixed first CRUD

--- classes/user.php
+++ classes/user.php
@@ -1,62 +1,68 @@
 <?php
-require_once '../config/database.php';
+
+require_once 'database/database.php';
+
 class User {
-     private $conn;
+    private $conn;
 
-     //Constructor
-     public function __construct(){
-         $database = new Database();
-         $db = $database->dbConnection();
-         $this->conn = $db;
-     }
+    // Constructor
+    public function __construct(){
+      $database = new Database();
+      $db = $database->dbConnection();
+      $this->conn = $db;
+    }
 
-     //Execute queries SQL
-     public function runQuery($sql){
-         $stat = $this->conn->prepare($sql);
-         return $stat;
-     }
 
-     // Insert
-     public function insert($name, $email, $password){
-         try{
-             $stmt = $this->conn->prepare("INSERT INTO users (name, email, password) VALUES(:name, :email, :password)");
-             $stmt->binparam(":name", $name);
-             $stmt->binparam(":email", $email);
-             $stmt->binparam(":password", $password);
-             $stmt->execute();
-         }catch(PDOException $e){
-             echo $e->getMessage();
-         }
-     }
+    // Execute queries SQL
+    public function runQuery($sql){
+      $stmt = $this->conn->prepare($sql);
+      return $stmt;
+    }
 
-     // Update
-     public function update($name, $email, $password, $id){
+    // Insert
+    public function insert($name, $email){
+      try{
+        $stmt = $this->conn->prepare("INSERT INTO users (name, email) VALUES(:name, :email)");
+        $stmt->bindparam(":name", $name);
+        $stmt->bindparam(":email", $email);
+        $stmt->execute();
+        return $stmt;
+      }catch(PDOException $e){
+        echo $e->getMessage();
+      }
+    }
+
+
+    // Update
+    public function update($name, $email, $id){
         try{
-            $stmt = $this->conn->prepare("UPDATE users SET name = :name, email = :email, password = :password WHERE id = :id");
-            $stmt->binparam(":name", $name);
-            $stmt->binparam(":email", $email);
-            $stmt->binparam(":password", $password);
-            $stmt->binparam(":id", $id);
-            $stmt->execute();
-            return $stmt;
-         }catch(PDOException $e){
-             echo $e->getMessage();
-         }
-     }
-
-     // Delete
-     public function delete($id){
-         try{
-             $stmt = $this->conn->prepare("DELETE FROM users WHERE id = :id");
-             $stmt->binparam(":id", $id);
-             return $stmt;
-         }catch(PDOException $e){
-            echo $e->getMessage();
+          $stmt = $this->conn->prepare("UPDATE users SET name = :name, email = :email WHERE id = :id");
+          $stmt->bindparam(":name", $name);
+          $stmt->bindparam(":email", $email);
+          $stmt->bindparam(":id", $id);
+          $stmt->execute();
+          return $stmt;
+        }catch(PDOException $e){
+          echo $e->getMessage();
         }
-     }
+    }
 
-     // Redirect URL method
-     public function redirect($url){
-         header("Location: $url");
-     }
+
+    // Delete
+    public function delete($id){
+      try{
+        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = :id");
+        $stmt->bindparam(":id", $id);
+        $stmt->execute();
+        return $stmt;
+      }catch(PDOException $e){
+          echo $e->getMessage();
+      }
+    }
+
+    // Redirect URL method
+    public function redirect($url){
+      header("Location: $url");
+    }
 }
+?>
